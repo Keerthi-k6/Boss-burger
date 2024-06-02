0a2f2e195aaaa676c455fb2dc9c885224f5fa36d
Keerthi-k6e <keerthisree_k@srmap.edu.in>
2024-06-02 21:35:57 +0530

    commit

--- src/components/InputContainer/InputContainer.jsx
+++ src/components/InputContainer/InputContainer.jsx
@@ -1,8 +1,8 @@
 import  styles from './InputContainer.module.css'
 
-export default function InputContainer({label,bgColor,children}) {
+export default function InputContainer({label,children}) {
     return (
-        <div className={styles.container} style={{backgroundColor:bgColor}}>
+        <div className={styles.container} >
              <label className={styles.label}>{label}</label>
              <div className={styles.content}>{children}</div>
         </div>
